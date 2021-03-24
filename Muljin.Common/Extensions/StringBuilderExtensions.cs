51993ea02fbcd47957ffd7632c86ae32622ac852
Mahmoud Swehli <mahmoud@muljin.com>
2021-03-24 15:43:27 +0000

    Addded stirng builder extensions

---
+++ Muljin.Common/Extensions/StringBuilderExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System
+{
+    public static class StringBuilderExtensions
+    {
+        /// <summary>
+        /// Appends all none null or whitespace strings to a string builder
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="seperator"></param>
+        public static void AppendNoneEmpty(this StringBuilder builder, string seperator, params string[] args)
+        {
+            bool first = true;
+            foreach(var a in args)
+            {
+                if (!String.IsNullOrWhiteSpace(a))
+                {
+                    //add seperator if not first
+                    if (!first && !String.IsNullOrEmpty(seperator))
+                    {
+                        builder.Append(seperator);
+                    }
+
+                    builder.Append(a);
+                    first = false;
+                }
+            }
+        }
+    }
+}
---
+++ Tests/Muljin.Common.Tests/StringBuilderExtensionTests.cs
@@ -0,0 +1,75 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Muljin.Common.Tests
+{
+    public class StringBuilderExtensionTests
+    {
+
+        [Test]
+        public void GivenNothingDoesNothing()
+        {
+            var sb = new StringBuilder();
+            sb.Append("test");
+
+            sb.AppendNoneEmpty(", ");
+
+            Assert.AreEqual("test", sb.ToString());
+        }
+
+        [Test]
+        public void GivenEmptySeperatorJoins()
+        {
+            var sb = new StringBuilder();
+            sb.AppendNoneEmpty("", "Test1", "Test2", "Test3 ", "Test4");
+            Assert.AreEqual("Test1Test2Test3 Test4", sb.ToString());
+        }
+
+        [Test]
+        public void GivenSeperatorAndNoneDoesNothing()
+        {
+            var sb = new StringBuilder();
+            var sb2 = new StringBuilder();
+
+            sb.Append("Test");
+            
+            sb.AppendNoneEmpty(", ");
+            sb2.AppendNoneEmpty(", ");
+
+            Assert.AreEqual("Test", sb.ToString());
+            Assert.AreEqual("", sb2.ToString());
+        }
+
+        [Test]
+        public void GivenSeperatorAndEmptyDoesNothing()
+        {
+            var sb = new StringBuilder();
+            var sb2 = new StringBuilder();
+
+            sb.Append("Test");
+
+            sb.AppendNoneEmpty(", ", "", " ", "   ");
+            sb2.AppendNoneEmpty(", ", "", " ", "    ");
+
+            Assert.AreEqual("Test", sb.ToString());
+            Assert.AreEqual("", sb2.ToString());
+        }
+
+        [Test]
+        public void GivenSeperatorAndItemsCorrectlyJoins()
+        {
+            var sb = new StringBuilder();
+            var sb2 = new StringBuilder();
+
+            sb.Append("Test");
+
+            sb.AppendNoneEmpty(", ", "Test1", "Test2", "Test3");
+            sb2.AppendNoneEmpty(", ", "Test1", "Test2", "Test3");
+
+            Assert.AreEqual("TestTest1, Test2, Test3", sb.ToString());
+            Assert.AreEqual("Test1, Test2, Test3", sb2.ToString());
+        }
+    }
+}
