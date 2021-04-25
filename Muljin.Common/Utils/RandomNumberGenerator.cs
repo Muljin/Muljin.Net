54a7e5be6b902a9429568c408f7134d8c6b29a62
Mahmoud Swehli <mahmoud@muljin.com>
2021-04-25 10:46:38 +0100

    added new random number generator util

---
+++ Muljin.Common/Utils/RandomNumberGenerator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Muljin.Utils
+{
+    public class RandomNumberGenerator : IDisposable
+    {
+        private RNGCryptoServiceProvider _rngCryptoService = new RNGCryptoServiceProvider();
+
+
+        public void Dispose()
+        {
+            _rngCryptoService.Dispose();
+        }
+
+        /// <summary>
+        /// Generate a random integer
+        /// </summary>
+        /// <param name="max">Maximum value. Cannot be higher that int.MaxValue</param>
+        /// <param name="min">Minimum value. Cannoy be less than int.MinimumValue</param>
+        /// <returns></returns>
+        public int Generate(int max, int min)
+        {
+            if(max < min)
+            {
+                throw new ArgumentException("Max cannot be less than min");
+            }
+            var bytes = new byte[4];
+            _rngCryptoService.GetBytes(bytes);
+            var val = BitConverter.ToUInt32(bytes, 0);
+            var res = (int)Math.Ceiling(min + ( (decimal)val / (decimal)UInt32.MaxValue * max ));
+            return res;
+        }
+    }
+}
---
+++ Tests/Muljin.Common.Tests/RandomNumberGeneratorTests.cs
@@ -0,0 +1,51 @@
+﻿using Muljin.Utils;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Muljin.Common.Tests
+{
+    public class RandomNumberGeneratorTests
+    {
+
+        [Test]
+        public void NumbersStayBetweenMaxAndMin()
+        {
+            int max = 874749;
+            int min = -2323;
+            using (var rn = new RandomNumberGenerator())
+            {
+                for(var i = 0; i<1000; i++)
+                {
+                    var a = rn.Generate(max,min);
+                    Assert.LessOrEqual(a, max);
+                    Assert.GreaterOrEqual(a, min);
+                }
+            }
+        }
+
+        [Test]
+        public void RandomnessDistributionShouldBeEqual()
+        {
+            int max = 874749;
+            int min = -2323;
+
+            decimal runningAvg = 0;
+
+            using (var rn = new RandomNumberGenerator())
+            {
+                for (var i = 1; i < 100000; i++)
+                {
+                    var a = rn.Generate(max, min);
+                    runningAvg += (a - runningAvg) / i;
+                }
+            }
+
+            var avg = (max + min) / 2m;
+
+            Assert.IsTrue(Math.Abs(runningAvg - avg) < (avg * 0.05m));
+
+        }
+    }
+}
