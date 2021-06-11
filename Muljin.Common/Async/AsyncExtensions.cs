e878094dee0ef00eda9058e0b88427e9d2581d76
Mahmoud Swehli <mahmoud@muljin.com>
2021-06-11 15:47:52 +0100

    added more async extensions

--- Muljin.Common/Async/AsyncExtensions.cs
+++ Muljin.Common/Async/AsyncExtensions.cs
@@ -21,5 +21,19 @@ namespace Muljin
             await Task.WhenAll(t1, t2, t3);
             return (t1.Result, t2.Result, t3.Result);
         }
+
+        public static async Task<(T1, T2, T3, T4)> WhenAllReturnAsync<T1, T2, T3, T4>(Task<T1> t1, Task<T2> t2, Task<T3> t3, 
+            Task<T4> t4)
+        {
+            await Task.WhenAll(t1, t2, t3, t4);
+            return (t1.Result, t2.Result, t3.Result, t4.Result);
+        }
+
+        public static async Task<(T1, T2, T3, T4, T5)> WhenAllReturnAsync<T1, T2, T3, T4, T5>(Task<T1> t1, Task<T2> t2, Task<T3> t3,
+            Task<T4> t4, Task<T5> t5)
+        {
+            await Task.WhenAll(t1, t2, t3, t4);
+            return (t1.Result, t2.Result, t3.Result, t4.Result, t5.Result);
+        }
     }
 }
