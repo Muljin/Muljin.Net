86b5cbaa97a2cdfc3904c50494e4e2ef25b175cb
Mahmoud Swehli <mahmoud@muljin.com>
2021-08-03 20:36:45 +0200

    added new overload for 6 async waits

--- Muljin.Common/Async/AsyncExtensions.cs
+++ Muljin.Common/Async/AsyncExtensions.cs
@@ -32,8 +32,16 @@ namespace Muljin
         public static async Task<(T1, T2, T3, T4, T5)> WhenAllReturnAsync<T1, T2, T3, T4, T5>(Task<T1> t1, Task<T2> t2, Task<T3> t3,
             Task<T4> t4, Task<T5> t5)
         {
-            await Task.WhenAll(t1, t2, t3, t4);
+            await Task.WhenAll(t1, t2, t3, t4, t5);
             return (t1.Result, t2.Result, t3.Result, t4.Result, t5.Result);
+        }
+
+        public static async Task<(T1, T2, T3, T4, T5, T6)> WhenAllReturnAsync<T1, T2, T3, T4, T5, T6>(
+            Task<T1> t1, Task<T2> t2, Task<T3> t3,
+            Task<T4> t4, Task<T5> t5, Task<T6> t6)
+        {
+            await Task.WhenAll(t1, t2, t3, t4, t5, t6);
+            return (t1.Result, t2.Result, t3.Result, t4.Result, t5.Result, t6.Result);
         }
     }
 }
