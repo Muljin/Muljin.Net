0be2cc259b24370ac25114df5b6c4d222985fd8d
Mahmoud Swehli <mahmoud@muljin.com>
2022-02-20 09:33:55 +0100

    added new exception and updated to net6

---
+++ Muljin.Common/Exceptions/ServiceUnavailableException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Muljin.Exceptions
+{
+    internal class ServiceUnavailableException : MuljinException
+    {
+        public ServiceUnavailableException() { }
+
+        public ServiceUnavailableException(string message) : base(message) { }
+
+        public ServiceUnavailableException(string message, string errorCode) : base(message, errorCode) { }
+}
+}
