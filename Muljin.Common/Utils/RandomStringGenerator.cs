391b990b6ead2343d67e8cc55907769565ccb0f7
Mahmoud Swehli <mahmoud@muljin.com>
2023-05-24 13:55:37 +0800

    added random password generator

--- Muljin.Common/Utils/RandomStringGenerator.cs
+++ Muljin.Common/Utils/RandomStringGenerator.cs
@@ -22,7 +22,38 @@ namespace Muljin.Utils
 			return sb.ToString();
 		}
 
-		private static char GenerateChar(Random rnd, bool caseSensitive)
+        // <summary>
+        /// Generate a random password composed of special characters, numbers, capital and small letters
+        /// </summary>
+        /// <param name="categoryLength">length of character generated from a single category</param>
+        /// <returns></returns>
+        public static string GenerateRandomPassword(int categoryLength = 5)
+        {
+            //ascii
+            var categories = new (int, int)[]
+            {
+                //(start, length)
+                (32, 15),//special characters
+                (48, 9),//numbers
+                (65, 25),//capital letters
+                (97, 25)//small letters
+            };
+
+            var passwordBuilder = new StringBuilder(categoryLength * categories.Length);
+
+            foreach (var (start, len) in categories)
+            {
+                for (int i = 0; i < categoryLength; i++)
+                {
+                    var c = (char)(byte)RandomNumberGenerator.GetSingle(start + len, start);
+                    passwordBuilder.Append(c);
+                }
+            }
+
+            return passwordBuilder.ToString();
+        }
+
+        private static char GenerateChar(Random rnd, bool caseSensitive)
 		{
 
 			// chars A = 65, Z = 90, a = 97, z = 122
