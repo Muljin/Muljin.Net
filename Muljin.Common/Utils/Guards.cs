b8597aafd9ef8d4afa832555a481c335b938489d
Mahmoud Swehli <mahmoud@muljin.com>
2024-02-17 18:36:59 +0000

    updates to common library

--- Muljin.Common/Utils/Guards.cs
+++ Muljin.Common/Utils/Guards.cs
@@ -42,24 +42,6 @@ namespace Muljin.Utils
         }
 
         /// <summary>
-        /// Ensure a string only consists of numbers
-        /// </summary>
-        /// <param name="obj"></param>
-        /// <param name="paramName"></param>
-        /// <param name="message"></param>
-        /// <exception cref="ArgumentException"></exception>
-        public static void Numeric(string obj, string paramName = null, string message = null)
-        {
-            foreach(var o in obj)
-            {
-                if (!Char.IsDigit(o))
-                {
-                    throw new ArgumentException(message ?? "Invalid input. Input must consist of numbers only", paramName);
-                }
-            }
-        }
-
-        /// <summary>
         /// Check value is greater than limit
         /// </summary>
         /// <param name="value"></param>
@@ -74,15 +56,70 @@ namespace Muljin.Utils
         }
 
         /// <summary>
+        /// Validate an enum is defiend is not the default 0 (e.g. None) enum
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        /// <param name="paramName"></param>
+        /// <param name="message"></param>
+        /// <exception cref="Exception"></exception>
+        /// <exception cref="ArgumentException"></exception>
+        public static void EnumDefined<T>(T obj, string paramName = null, string message = null) where T : struct, Enum
+        {
+            if(!typeof(T).IsEnum){
+                throw new Exception("Invalid type used");
+            }
+
+            if(!Enum.IsDefined<T>(obj))
+            {
+                throw new ArgumentException("Enum not defined", paramName);
+            }
+
+            if( ((IConvertible)obj).ToInt32(null) == 0){
+                throw new ArgumentException("Enum cannot be default value", paramName);
+            }
+        }
+
+        /// <summary>
+        /// Validate a specified length for a string, ignoring whitespaces and trimming
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="min"></param>
+        /// <param name="max"></param>
+        public static void Length(string input, int min = -1, int max = -1, bool allowWhiteSpace = false)
+        {
+            if(allowWhiteSpace)
+            {
+                ArgumentNullException.ThrowIfNull(input, nameof(input));
+            }else{
+                ArgumentNullException.ThrowIfNullOrWhiteSpace(input, nameof(input));
+                input = input.Trim();
+            }
+
+            var len = input.Length;
+
+            //check min length
+            if(min > -1 && len < min)
+            {
+                throw new ArgumentException("Invalid input length.");
+            }
+
+            //check max length
+            if(max >-1 && len > max){
+                throw new ArgumentException("Invalid input length.");
+            }
+        }
+
+        /// <summary>
         /// Check value is less than given limit
         /// </summary>
         /// <param name="value"></param>
         /// <param name="limit"></param>
-        public static void LessThan(int value, int limit, string paramName, string message = null) 
+        public static void LessThan(int value, int limit, string paramName = null, string message = null) 
         {
             if(value >= limit)
             {
-                throw new ArgumentException(message ?? $"Invalid parameter. {paramName} must be less than {limit}", paramName);
+                throw new ArgumentException(message ?? $"Invalid parameter. {paramName ?? string.Empty} must be less than {limit}", paramName);
             }
         }
 
@@ -112,7 +149,7 @@ namespace Muljin.Utils
         /// <param name="obj"></param>
         /// <param name="paramName"></param>
         /// <param name="message"></param>
-        public static void NotDefault(string obj, string paramName, string message = null)
+        public static void NotDefault(string obj, string paramName = null, string message = null)
         {
             if (obj == null)
             {
@@ -166,5 +203,24 @@ namespace Muljin.Utils
                 throw new ArgumentException(message ?? "Guid cannot be empty", paramName);
             }
         }
+        
+        /// <summary>
+        /// Ensure a string only consists of numbers
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="paramName"></param>
+        /// <param name="message"></param>
+        /// <exception cref="ArgumentException"></exception>
+        public static void Numeric(string obj, string paramName = null, string message = null)
+        {
+            foreach(var o in obj)
+            {
+                if (!Char.IsDigit(o))
+                {
+                    throw new ArgumentException(message ?? "Invalid input. Input must consist of numbers only", paramName);
+                }
+            }
+        }
+        
     }
 }
