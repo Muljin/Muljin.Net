0a8ce2834bb0fd821e4f81294e57e0cb56d1df80
Mahmoud Swehli <mahmoud@muljin.com>
2024-07-10 14:56:01 +0100

    new exception for forbidden

---
+++ Muljin.Common/Exceptions/ForbiddenException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Muljin.Exceptions
+{
+    /// <summary>
+    /// Exceptions thrown directly against user actions performed primarily via API calls.
+    /// </summary>
+    public class ForbiddenException : MuljinException
+    {
+        public ForbiddenException() : base() { }
+
+        public ForbiddenException(string message) : base(message) { }
+
+        public ForbiddenException(string message, string errorCode) : base(message, errorCode)
+        {
+        }
+
+
+    }
+}
